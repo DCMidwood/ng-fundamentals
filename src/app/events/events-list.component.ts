e21c764f105e7d4f42f4fcafb1e9c1d405e9ba8a
Daniel@ThinkPad <daniel.c-m@outlook.com>
2021-12-08 11:19:01 +0100

    end of comm with parents

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,4 +1,3 @@
-import { templateJitUrl } from '@angular/compiler';
 import { Component } from '@angular/core'
 
 @Component({
@@ -6,23 +5,12 @@ import { Component } from '@angular/core'
     template: `
     <div>
         <h1>Upcoming Angular Events</h1>
-        <div class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
-            <div>Price: \${{event.price}}</div>
-            <div>
-                <span>Location: {{event.location.address}}</span>
-                <span>&nbsp;</span>
-                <span>{{event.location.city}}, {{event.location.country}}</span>
-            </div> 
-        </div> 
+        <hr/>
     </div>
     `
 })
-
 export class EventsListComponent {
-    event={
+    event1={
         id:1,
         name:'Angular Connect',
         date:'26/9/2036',
@@ -35,5 +23,7 @@ export class EventsListComponent {
             country:'England'
             }
     }
-
+    handleEventClicked(data: any){
+        console.log('recieved:', data)
+    }
 }
