e977c4af2b02ab4c7dab7ffdc5e3a30ba546ef82
Daniel Caparros <Daniel.caparros@dnv.com>
2021-12-09 22:02:54 +0100

    Conditional stylings

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,13 @@ import { Component , EventEmitter, Input, Output } from "@angular/core";
     `<div class="well hoverwell thumbnail">
         <h2>{{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div>Time: {{event?.time}}</div>
+        <! –– <div [ngClass]="getStartTimeStyle" [ngSwitch]="event?.time">
+        <div [ngStyle]="getStartTimeStyle()"  [ngSwitch]="event?.time">
+            Time: {{event?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span *ngSwitchDefault > (Normal Start)</span>
+        </div>            
         <div>Price: \${{event?.price}}</div>
         <div *ngIf="event?.location">
             <span>Location: {{event?.location?.address}}</span>
@@ -17,7 +23,7 @@ import { Component , EventEmitter, Input, Output } from "@angular/core";
         </div>
     </div> `,
     styles:[`
-    .thumbnail { min-height: 210px; }
+        .thumbnail { min-height: 210px; }
         .pad-left { margin-left: 10px;}
         .well div { color: #bbb; }
     `]
@@ -29,5 +35,11 @@ export class EventThumbnailComponent{
 
     logFoo(){
         console.log('foo')
+    }
+
+    getStartTimeStyle(): any {
+        if ( this.event && this.event.time === '8:00 am')
+            return {color: '#005300', 'font-weight': 'bold'}
+        return {color: '#bfb'}
     }
 }
